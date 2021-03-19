0b712c29dc56614784b6cc42b5a1a4ea27ed7486
Dao Hong Vinh <60530946+lambiengcode@users.noreply.github.com>
2021-03-19 19:48:12 +0700

    done switch and storage theme

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_template/src/public/constant.dart';
+import 'package:flutter_getx_template/src/theme/theme_service.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,6 +11,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.refresh,
+          size: width / 16.0,
+        ),
+        onPressed: () {
+          ThemeService().changeThemeMode();
+        },
+      ),
       body: Container(),
     );
   }
